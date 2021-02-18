0607feb0e0defa3dfa4ff8c862d71f419d0d3fea
GeneralPoxter <generalpoxter@gmail.com>
2021-02-18 02:30:28 -0500

    explanatory comments

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -4,23 +4,33 @@ import (
 	"strconv"
 )
 
+// Struct example
 type cond struct {
 	key int
 	str string
 }
 
+// Variadic function example
 func fizzbuzz(k, n int, conds ...cond) (out []string) {
+	// Initialize string slice of size n
 	out = make([]string, n)
+
+	// Construct for loop
 	for i := 0; i < n; i++ {
+		// Declare variables
 		var str string
 		x := k + i
+
+		// Iterate through range
 		for _, cond := range conds {
+			// If statement example
 			if x%cond.key == 0 {
 				str = str + cond.str
 			}
 		}
 
 		if len(str) == 0 {
+			// Int to string conversion
 			str = strconv.Itoa(x)
 		}
 
--- server.go
+++ server.go
@@ -9,28 +9,35 @@ import (
 	"strings"
 )
 
+// Web server example
 func main() {
+	// Serve web page with handler
 	fileServer := http.FileServer(http.Dir("./public"))
 	http.Handle("/", fileServer)
 	http.HandleFunc("/fizzbuzz", fizzbuzzHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
+	// Error handling example
 	if err := http.ListenAndServe(GetPort(), nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
+// Request handler example
 func fizzbuzzHandler(w http.ResponseWriter, r *http.Request) {
+	// Check request path
 	if r.URL.Path != "/fizzbuzz" {
 		http.Error(w, "404 not found.", http.StatusNotFound)
 		return
 	}
 
+	// Check request method
 	if r.Method != "GET" {
 		http.Error(w, "Method is not supported.", http.StatusNotFound)
 		return
 	}
 
+	// Check request query parameters
 	start, err := strconv.Atoi(r.URL.Query().Get("start"))
 	if err != nil || start < -10000 || start > 10000 {
 		fmt.Fprintf(w, "Start value is not an integer between -10000 and 10000")
@@ -43,11 +50,13 @@ func fizzbuzzHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Respond with fizzbuzz output
 	fmt.Fprintf(w, strings.Join(fizzbuzz(start, number,
 		cond{3, "fizz"},
 		cond{5, "buzz"}), " "))
 }
 
+// Port function for Heroku deployment
 func GetPort() string {
 	port := os.Getenv("PORT")
 	if port == "" {
